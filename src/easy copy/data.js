d882fc9fbbb770e0d85640ba2130a916cb22e478
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2024-12-13 16:26:28 +0100

    update

---
+++ src/easy copy/data.js
@@ -0,0 +1,21 @@
+window.audios = [
+  {
+    title:
+      "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
+    artist: "Diego Fernandes",
+    cover: "files/como-comecei.jpg",
+    file: "files/como-comecei.mp3"
+  },
+  {
+    title: "5 dicas para uma carreira sólida como programador",
+    artist: "Diego Fernandes",
+    cover: "files/5-dicas-para-uma-carreira-solida-como-programador.jpg",
+    file: "files/5-dicas-para-uma-carreira-solida-como-programador.mp3"
+  },
+  {
+    title: "Júnior Pleno ou Sênior, qual a diferença?",
+    artist: "Diego Fernandes",
+    cover: "files/junior-pleno-ou-senior-qual-a-diferenca.jpg",
+    file: "files/junior-pleno-ou-senior-qual-a-diferenca.mp3"
+  }
+];
